c8e3d6b33a5e8da76433652f5e3ada64f220eb96
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-31 13:30:42 -0500

    Names Stretch Goal complete runtime: 0.00701451301574707 seconds

--- names/names.py
+++ names/names.py
@@ -4,25 +4,28 @@ from binary_search_tree import BinarySearchTree
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+# names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = set(line.strip() for line in f)
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = set(line.strip() for line in f)
+# names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-tree = BinarySearchTree(names_2.pop())
-for name in names_2:
-    tree.insert(name)
-
-duplicates = []
-for name_1 in names_1:
-    if tree.contains(name_1):
-        # Old version ran at O(n^2) Time
-        # BST should reduce that to O(n log n)
-        #     for name_2 in names_2:
-        #         if name_1 == name_2:
-        duplicates.append(name_1)
+# print(names_2)
+# tree = BinarySearchTree(names_2.pop())
+# for name in names_2:
+#     tree.insert(name)
+# print(names_1 & names_2)
+duplicates = [*names_1 & names_2]
+# for name_1 in names_1:
+#     if tree.contains(name_1):
+#         # Old version ran at O(n^2) Time
+#         # BST should reduce that to O(n log n)
+#         #     for name_2 in names_2:
+#         #         if name_1 == name_2:
+#         duplicates.append(name_1)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
