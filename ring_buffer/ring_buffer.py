f7ed75b2268b5568efcadd88956ba65d3260ca64
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-31 11:49:01 -0500

    Ring Buffer complete and tested

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,36 +8,38 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        # newItem = ListNode(item)
         if self.current == None:
-            self.storage.add_to_tail(item)
-            self.current = item
+            self.current = self.storage.add_to_tail(item)
         elif len(self.storage) < self.capacity:
             self.storage.add_to_tail(item)
         elif len(self.storage) == self.capacity:
+            # print("CURR", self.current)
             self.current.value = item
             if self.current.next:
                 self.current = self.current.next
             else:
-                self.current = self.head
+                self.current = self.storage.head
+            # print("NEW CURR", self.current)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
         curr = self.storage.head
         while curr:
-            # print("CURR", curr.value)
             list_buffer_contents.append(curr.value)
             curr = curr.next
-        self.current = None
-        self.storage = DoublyLinkedList()
-        print("CONT", list_buffer_contents)
+        # self.current = None
+        # self.storage = DoublyLinkedList()
         return list_buffer_contents
 
 
 # buffer = RingBuffer(5)
 # buffer.append('a')
 # buffer.append('b')
+# buffer.append('c')
+# buffer.append('d')
+# buffer.append('e')
+# buffer.append('f')
 # print("BUFF", buffer.get())
 
 # ----------------Stretch Goal-------------------
