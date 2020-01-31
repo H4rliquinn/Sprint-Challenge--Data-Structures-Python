df3af044e39ce65637d7494a6e91da3b40e726d2
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-31 12:14:44 -0500

    Names runtime optimized to .04 seconds.

--- names/names.py
+++ names/names.py
@@ -11,21 +11,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-names_3 = ['Nathen Bishop',
-           'Lilly Pineda',
-           'Dominique Beck',
-           'Anabella Atkinson']
 tree = BinarySearchTree(names_2.pop())
 for name in names_2:
     tree.insert(name)
-print(tree)
 
 duplicates = []
-# for name_1 in names_1:
-#     if tree.contains(name_1):
-#         #     for name_2 in names_2:
-#         #         if name_1 == name_2:
-#         duplicates.append(name_1)
+for name_1 in names_1:
+    if tree.contains(name_1):
+        #     for name_2 in names_2:
+        #         if name_1 == name_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
